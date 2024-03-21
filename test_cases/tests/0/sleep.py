1e6c850ca1b0ada02cbe828b535ba8e8d4e160c5
Joseda8 <jd.montoya.s8@gmail.com>
2024-03-21 11:46:09 +0100

    Add a new test

---
+++ test_cases/tests/0/sleep.py
@@ -0,0 +1,19 @@
+"""
+This benchmark sleeps to measure the overhead caused by the profiler.
+
+Benchmark Steps:
+1. Zzzz.
+"""
+
+import time
+import os
+
+from src.client_interface import set_tag, set_output_filename
+
+# Set output filename
+set_output_filename(filename=f"{os.path.splitext(os.path.basename(__file__))[0]}")
+
+# Idle time to ensure that the whole program is profiled
+set_tag("start_sleep")
+time.sleep(10)
+set_tag("finish_sleep")
