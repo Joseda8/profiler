fe286b81b67f76f160b9a250da459d4736f98a72
Joseda8 <jd.montoya.s8@gmail.com>
2024-03-22 14:07:29 +0100

    Add version beta of method to extract dominant core

--- test_cases/projects/pandas/process_results/main.py
+++ test_cases/projects/pandas/process_results/main.py
@@ -33,6 +33,30 @@ class FileStats:
         """
         return self._df_stats.index[self._df_stats["label"] == label].tolist()
 
+    def _get_df_between_labels(self, start_label: str, finish_label: str) -> Optional[pd.DataFrame]:
+        """
+        Extract the DataFrame between two labels.
+
+        Args:
+            start_label (str): The starting label.
+            finish_label (str): The finishing label.
+
+        Returns:
+            Optional[pd.DataFrame]: DataFrame containing rows between start_label and finish_label indices.
+        """
+        # Find the indices of start_label and finish_label
+        start_indices = self._find_label_indices(start_label)
+        finish_indices = self._find_label_indices(finish_label)
+
+        # Ensure start_label and finish_label are found in the dataframe
+        if not start_indices or not finish_indices:
+            return None
+
+        # Extract rows between start_label and finish_label indices
+        df_between_labels = self._df_stats.loc[start_indices[0]:finish_indices[0]]
+
+        return df_between_labels
+
     def get_times(self) -> Dict[str, float]:
         """
         Calculate time differences between start and finish tasks.
@@ -72,16 +96,8 @@ class FileStats:
             the average CPU usage, average virtual memory usage, average RAM usage,
             and average swap usage between the labels. If either label is not found, returns None.
         """
-        # Find the indices of start_label and finish_label
-        start_indices = self._find_label_indices(start_label)
-        finish_indices = self._find_label_indices(finish_label)
-
-        # Ensure start_label and finish_label are found in the dataframe
-        if not start_indices or not finish_indices:
-            return None
-
         # Extract rows between start_label and finish_label indices
-        df_between_labels = self._df_stats.loc[start_indices[0]:finish_indices[0]]
+        df_between_labels = self._get_df_between_labels(start_label=start_label, finish_label=finish_label)
 
         # Calculate the average CPU usage, virtual memory usage, RAM usage, and swap usage for the relevant rows
         average_cpu_usage = df_between_labels["cpu_usage"].mean()
@@ -119,16 +135,8 @@ class FileStats:
             the minimum and maximum values of virtual memory usage, RAM usage,
             and swap usage between the labels. If either label is not found, returns None.
         """
-        # Find the indices of start_label and finish_label
-        start_indices = self._find_label_indices(start_label)
-        finish_indices = self._find_label_indices(finish_label)
-
-        # Ensure start_label and finish_label are found in the dataframe
-        if not start_indices or not finish_indices:
-            return None
-
         # Extract rows between start_label and finish_label indices
-        df_between_labels = self._df_stats.loc[start_indices[0]:finish_indices[0]]
+        df_between_labels = self._get_df_between_labels(start_label=start_label, finish_label=finish_label)
 
         # Calculate the minimum and maximum values of virtual memory usage, RAM usage, and swap usage for the relevant rows
         min_virtual_memory_usage = df_between_labels["virtual_memory_usage"].min()
@@ -153,9 +161,60 @@ class FileStats:
         min_max_stats = self.get_min_max_memory_stats(start_label=LABEL_START_PROGRAM, finish_label=LABEL_FINISH_PROGRAM)
         return min_max_stats
 
+    def track_dominant_core_changes_between_labels(self, start_label: str, finish_label: str) -> Optional[float]:
+        """
+        Track changes in the dominant core between specific labels and calculate the average duration between changes.
+
+        Args:
+            start_label (str): The starting label.
+            finish_label (str): The finishing label.
+
+        Returns:
+            Optional[float]: The average duration between changes in the dominant core between the specified labels in seconds.
+        """
+        # Extract rows between start_label and finish_label indices
+        df_between_labels = self._get_df_between_labels(start_label=start_label, finish_label=finish_label)
+
+        # Find dominant core
+        dominant_core_changes = 0
+        current_dominant_core = None
+        for _, row in df_between_labels.iterrows():
+            dominant_core = self._find_dominant_core(row=row)
+            if current_dominant_core and dominant_core:
+                if dominant_core != current_dominant_core:
+                    current_dominant_core = dominant_core
+                    dominant_core_changes += 1
+            else:
+                current_dominant_core = dominant_core
+        return dominant_core_changes
+
+
+    def _find_dominant_core(self, row: pd.Series, threshold: int = 70) -> Optional[int]:
+        """
+        Find the dominant core in a given row.
+
+        Args:
+            row (pd.Series): A pandas Series representing a row in the DataFrame.
+            threshold (int, optional): The threshold for determining dominance. Defaults to 70.
+
+        Returns:
+            Optional[int]: The index of the dominant core if found, None otherwise.
+        """
+        core_columns = ['core_1_usage', 'core_2_usage', 'core_3_usage', 'core_4_usage', 'core_5_usage', 'core_6_usage']
+        max_usage = 0
+        dominant_core = None
+        for i, core in enumerate(core_columns):
+            if row[core] > max_usage and all(row[core] - row[other_core] >= threshold for other_core in core_columns if other_core != core):
+                max_usage = row[core]
+                dominant_core = i + 1
+
+        return dominant_core
+
+
 # Example usage:
 file_path: str = "/home/josemontoya/work/profiler/results/preprocessed/no_conversion_1000000_20240322_113415_stats.csv"
 file_stats: FileStats = FileStats(file_path)
 print(file_stats.get_times())
 print(file_stats.get_average_stats())
 print(file_stats.get_min_max_stats())
+print(file_stats.track_dominant_core_changes_between_labels(start_label=LABEL_START_PROGRAM, finish_label=LABEL_FINISH_PROGRAM))
