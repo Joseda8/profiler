3a13aa8d49b3ead35eb02d6e1bb7e6fe64d304be
Joseda8 <jd.montoya.s8@gmail.com>
2024-05-02 19:43:26 +0200

    Include RAM in base memory usage test

--- test_cases/projects/general/1/base_memory.py
+++ test_cases/projects/general/1/base_memory.py
@@ -1,3 +1,4 @@
 import psutil
 process = psutil.Process()
 print(f"VMS (Virtual Memory Size): {process.memory_info().vms / (1024 * 1024):.2f} MB")
+print(f"RAM: {process.memory_info().rss / (1024 * 1024):.2f} MB")
