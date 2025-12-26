b69b9dbae907d23cfdc0928c3cb66415e1b4e7df
Joseda8 <jd.montoya.s8@gmail.com>
2025-12-25 18:06:41 -0600

    Add temperature measurements

--- src/system_stats_collector/const.py
+++ src/system_stats_collector/const.py
@@ -1,4 +1,4 @@
 # Values to measure
 KEYWORD_CPU_USAGE_PER_CORE = "cpu_usage_per_code"
 TEMPLATE_USAGE_PER_CORE = "core_{core_idx}_usage"
-VALUES_TO_MEASURE = ["uptime", "cpu_usage", KEYWORD_CPU_USAGE_PER_CORE, "virtual_memory_usage", "ram_usage", "swap_usage", "energy_consumed"]
+VALUES_TO_MEASURE = ["uptime", "cpu_usage", KEYWORD_CPU_USAGE_PER_CORE, "virtual_memory_usage", "ram_usage", "swap_usage", "energy_consumed", "cpu_temperature"]
