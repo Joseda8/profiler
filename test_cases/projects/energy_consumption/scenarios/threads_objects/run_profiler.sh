4051943883f8cba887e55c7839eefa207ddf2791
Joseda8 <jd.montoya.s8@gmail.com>
2026-01-03 13:21:18 -0600

    Increase sleep time between energy consumption tests to 5 seconds

--- test_cases/projects/energy_consumption/scenarios/threads_objects/run_profiler.sh
+++ test_cases/projects/energy_consumption/scenarios/threads_objects/run_profiler.sh
@@ -14,17 +14,17 @@ workers=(1 2 4 6 8 12)
 # JSON parse
 for w in "${workers[@]}"; do
   python3 -m src.main --file_to_run test_cases.projects.energy_consumption.scenarios.threads_objects.0.json_parse --is_module --script_args --num_records 3000000 --num_workers "$w" --run_idx "$RUN_ID"
-  sleep 1
+  sleep 5
 done
 
 # Text tokenize
 for w in "${workers[@]}"; do
   python3 -m src.main --file_to_run test_cases.projects.energy_consumption.scenarios.threads_objects.1.text_tokenize --is_module --script_args --num_records 3000000 --num_workers "$w" --run_idx "$RUN_ID"
-  sleep 1
+  sleep 5
 done
 
 # Object lists
 for w in "${workers[@]}"; do
   python3 -m src.main --file_to_run test_cases.projects.energy_consumption.scenarios.threads_objects.2.object_lists --is_module --script_args --num_records 8000000 --num_workers "$w" --run_idx "$RUN_ID"
-  sleep 1
+  sleep 5
 done
