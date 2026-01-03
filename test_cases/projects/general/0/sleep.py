46b1281f5348d59f6afc3c19ea5f10590a7c807a
Joseda8 <jd.montoya.s8@gmail.com>
2026-01-03 13:20:41 -0600

    Modify sleep general case to measure its own energy as well

--- test_cases/projects/general/0/sleep.py
+++ test_cases/projects/general/0/sleep.py
@@ -8,10 +8,17 @@ Benchmark Steps:
 import time
 import os
 
+import psutil
+
 from src.client_interface import set_tag, set_output_filename
 
 # Set output filename
 set_output_filename(filename=f"{os.path.splitext(os.path.basename(__file__))[0]}")
+
+# Measure memory usage
+process = psutil.Process()
+print(f"VMS (Virtual Memory Size): {process.memory_info().vms / (1024 * 1024):.2f} MB")
+print(f"RAM: {process.memory_info().rss / (1024 * 1024):.2f} MB")
 
 # Sleep
 set_tag("start_sleep")
